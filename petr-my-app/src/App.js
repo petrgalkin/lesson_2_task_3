3f106d00478d285dddf8146e543a94de4f67fc4a
Petr Galkin <petr.galkin@gmail.com>
2024-12-15 00:44:50 +0300

    lesson 2 task 3 - completed homework

--- petr-my-app/src/App.js
+++ petr-my-app/src/App.js
@@ -1,59 +1,129 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import styles from "./App.module.css";
+import { useState } from "react";
 
-function App() {
-	const currentYear = new Date().getFullYear();
-	return React.createElement(
-		"div",
-		{ className: "App" },
-		React.createElement(
-			"header",
-			{ className: "App-header" },
-			React.createElement("img", {
-				src: logo,
-				className: "App-logo",
-				alt: "logo",
-			}),
-			React.createElement(
-				"p",
-				null,
-				"Edit ",
-				React.createElement("core", null, "src/App.js"),
-				" and save to reload."
-			),
-			React.createElement(
-				"a",
-				{
-					className: "App-link",
-					href: "https://reactjs.org",
-					target: "_blank",
-					rel: "noopener noreferrer",
-				},
-				"Learn React"
-			),
-			React.createElement("p", null, currentYear)
-		)
+export const App = () => {
+	const NUMS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+	// const manipulations = ["+", "-", "=", "C"];
+	const [operand1, setOperand1] = useState("");
+	const [operator, setOperator] = useState("");
+	const [operand2, setOperand2] = useState("");
+	const [resultDisplayed, setResultDisplayed] = useState(false);
+
+	const num = (number) => {
+		if (resultDisplayed) {
+			setOperand1(num);
+			setOperator("");
+			setOperand2("");
+			setResultDisplayed(false);
+		} else if (operator) {
+			setOperand2(operand2 + number);
+		} else {
+			setOperand1(operand1 + number);
+		}
+	};
+
+	const operation = (oper) => {
+		if (resultDisplayed) {
+			setResultDisplayed(false);
+		}
+		if (operand1 && !operand2) {
+			setOperator(oper);
+		}
+	};
+
+	const clear = () => {
+		setOperand1("");
+		setOperator("");
+		setOperand2("");
+		setResultDisplayed(false);
+	};
+
+	const equals = () => {
+		if (operand1 && operator && operand2) {
+			const result = calculateResult();
+			setOperand1(result.toString());
+			setOperator("");
+			setOperand2("");
+			setResultDisplayed(true);
+		}
+	};
+
+	const calculateResult = () => {
+		const num1 = Number(operand1);
+		const num2 = Number(operand2);
+		switch (operator) {
+			case "+":
+				return num1 + num2;
+			case "-":
+				return num1 - num2;
+			default:
+				return 0;
+		}
+	};
+
+	return (
+		<>
+			<header className={styles.header}>
+				<h1>Калькулятор</h1>
+			</header>
+			<main>
+				<article>
+					<div
+						className={
+							resultDisplayed ? styles.result : styles.display
+						}
+					>
+						<p>
+							{operand1}
+							{operator}
+							{operand2}
+						</p>
+					</div>
+
+					<div>
+						<ul className={styles.buttons}>
+							{NUMS.map((number) => {
+								return (
+									<li key={number}>
+										<button
+											className={styles.number}
+											onClick={() => num(number)}
+										>
+											{number}
+										</button>
+									</li>
+								);
+							})}
+						</ul>
+					</div>
+
+					<div className={styles.operations}>
+						<button
+							className={styles.operator}
+							onClick={() => {
+								operation("+");
+							}}
+						>
+							+
+						</button>
+						<button
+							className={styles.operator}
+							onClick={() => {
+								operation("-");
+							}}
+						>
+							-
+						</button>
+						<button className={styles.operator} onClick={equals}>
+							=
+						</button>
+						<button className={styles.operator} onClick={clear}>
+							C
+						</button>
+					</div>
+				</article>
+			</main>
+		</>
 	);
-	// Начало декларативного стиля
-	// <div className="App">
-	// 	<header className="App-header">
-	// 		<img src={logo} className="App-logo" alt="logo" />
-	// 		<p>
-	// 			Edit <code>src/App.js</code> and save to reload.
-	// 		</p>
-	// 		<a
-	// 			className="App-link"
-	// 			href="https://reactjs.org"
-	// 			target="_blank"
-	// 			rel="noopener noreferrer"
-	// 		>
-	// 			Learn React
-	// 		</a>
-	// 		<p>{currentYear}</p>
-	// 	</header>
-	// </div>
-	// Конец декларативного стиля
-}
-
-export default App;
+};
--- petr-my-app/src/App.test.js
+++ petr-my-app/src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import styles from "./App.module.css";
 
 test("renders learn react link", () => {
 	render(<App />);
